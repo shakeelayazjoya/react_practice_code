aea21c66d5a6c277d73b107f5aa7529d113f7160
shakeelayazjoya <shakeelayaz954@gmail.com>
2024-11-15 15:02:08 +0500

    bootstrap project

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <h1>this is my text</h1>
+      <button className="btn btn-warning">shakeel</button>
+    </>
+  );
+};
+
+export default App;
